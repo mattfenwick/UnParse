3dadb0bcc1eb1a0cfbc5064f708f21651f6eca76
Matt Fenwick <mfenwick100@gmail.com>
2013-06-25 18:02:08 -0400

    unit tests:  extend to cover all parser combinators (standard versions)

--- unparse/test/teststandard.py
+++ unparse/test/teststandard.py
@@ -15,9 +15,23 @@ def good(rest, state, result):
 
 class TestParser(u.TestCase):
     
+    def testFmap(self):
+        f = lambda x: x + 7
+        v1 = p.pure(3).fmap(f).parse('ab', 81)
+        self.assertEqual(good('ab', 81, 10), v1)
+        v2 = p.zero.fmap(f).parse('ab', 81)
+        self.assertEqual(m.zero, v2)
+        v3 = p.error('oops').fmap(f).parse('ab', 81)
+        self.assertEqual(m.error('oops'), v3)
+        
     def testPure(self):
         val = p.pure(3).parse('abc', 2)
         self.assertEqual(good('abc', 2, 3), val)
+    
+    def testBind(self):
+        two = p.item.bind(lambda x: p.literal(x))
+        self.assertEqual(two.parse(l('abcde'), {}), m.zero)
+        self.assertEqual(two.parse(l('aabcde'), {}), good(l('bcde'), {}, 'a'))
         
     def testPlus(self):
         g1, g2, b, e = p.pure(3), p.pure('hi'), p.zero, p.error('oops')
@@ -25,30 +39,46 @@ class TestParser(u.TestCase):
         self.assertEqual(b.plus(g2).parse('abc', 3), g2.parse('abc', 3))
         self.assertEqual(e.plus(g1).parse('abc', 4), e.parse('abc', 4))
     
-    def testBind(self):
-        two = p.item.bind(lambda x: p.literal(x))
-        self.assertEqual(two.parse(l('abcde'), {}), m.zero)
-        self.assertEqual(two.parse(l('aabcde'), {}), good(l('bcde'), {}, 'a'))
+    def testError(self):
+        v1 = p.error('uh-oh').parse('abc', 123)
+        self.assertEqual(m.error('uh-oh'), v1)
+    
+    def testMapError(self):
+        f = len
+        v1 = p.error('abcdef').mapError(f).parse('123abc', None)
+        self.assertEqual(m.error(6), v1)
+        v2 = p.zero.mapError(f).parse('123abc', None)
+        self.assertEqual(m.zero, v2)
+        v3 = p.pure(82).mapError(f).parse('123abc', None)
+        self.assertEqual(good('123abc', None, 82), v3)        
 
     def testPut(self):
         val = p.put('xyz')
         self.assertEqual(val.parse('abc', []), good('xyz', [], None))
-        
-    def testGet(self):
-        self.assertEqual(p.get.parse('abc', {}), good('abc', {}, 'abc'))
     
-    def testItem(self):
-        self.assertEqual(p.item.parse(l(range(5)), {}), good(l([1,2,3,4]), {}, 0))
+    def testPutState(self):
+        v1 = p.putState(29).parse('abc123', 2)
+        self.assertEqual(good('abc123', 29, None), v1)
+    
+    def testUpdateState(self):
+        v1 = p.updateState(lambda x: x * 4).parse('abc', 18)
+        self.assertEqual(good('abc', 72, None), v1)
+        
+    def testCheck(self):
+        val = p.get.check(lambda x: len(x) > 3)
+        self.assertEqual(val.parse('abcde', []), good('abcde', [], 'abcde'))
+        self.assertEqual(val.parse('abc', []), m.zero)
     
     def testLiteral(self):
         val = p.literal(3)
         self.assertEqual(val.parse(l([3,4,5]), {}), good(l([4,5]), {}, 3))
         self.assertEqual(val.parse(l([4,5]), {}), m.zero)
     
-    def testCheck(self):
-        val = p.get.check(lambda x: len(x) > 3)
-        self.assertEqual(val.parse('abcde', []), good('abcde', [], 'abcde'))
-        self.assertEqual(val.parse('abc', []), m.zero)
+    def testSatisfy(self):
+        v1 = p.satisfy(lambda x: x > 3).parse(l([1,2,3]), 'bye')
+        self.assertEqual(m.zero, v1)
+        v2 = p.satisfy(lambda x: x < 3).parse(l([1,2,3]), 'hi')
+        self.assertEqual(good(l([2,3]), 'hi', 1), v2)
     
     def testMany0(self):
         val = p.literal(3).many0()
@@ -64,6 +94,22 @@ class TestParser(u.TestCase):
         val = p.all([p.item, p.literal(2), p.literal(8)])
         self.assertEqual(val.parse(l([3,2,4]), {}), m.zero)
         self.assertEqual(val.parse(l([3,2,8,16]), {}), good(l([16]), {}, [3,2,8]))
+    
+    def testApp(self):
+        parser = p.app(lambda x,y,z: x + y * z, p.item, p.satisfy(lambda x: x > 2), p.item)
+        v1 = parser.parse(l([1,2,3,4,5]), 'hi')
+        self.assertEqual(m.zero, v1)
+        v2 = parser.parse(l([5,6,7,8,9]), 'bye')
+        self.assertEqual(good(l([8,9]), 'bye', 47), v2)
+        v3 = parser.parse(l([5,6]), 'goodbye')
+        self.assertEqual(m.zero, v3)
+    
+    def testOptional(self):
+        parser = p.literal(3).optional('blargh')
+        v1 = parser.parse(l([1,2,3]), 'hi')
+        self.assertEqual(good(l([1,2,3]), 'hi', 'blargh'), v1)
+        v2 = parser.parse(l([3,2,1]), 'bye')
+        self.assertEqual(good(l([2,1]), 'bye', 3), v2)
     
     def testSeq2R(self):
         val = p.literal(2).seq2R(p.literal(3))
@@ -91,4 +137,32 @@ class TestParser(u.TestCase):
         val = p.literal(2).commit('bag-agg')
         self.assertEqual(val.parse(l([2,3,4]), 'hi'), good(l([3,4]), 'hi', 2))
         self.assertEqual(val.parse(l([3,4,5]), 'hi'), m.error('bag-agg'))
+    
+    def testString(self):
+        parser = p.string('abc')
+        v1 = parser.parse(l('abcdef'), None)
+        self.assertEqual(good(l('def'), None, 'abc'), v1)
+        v2 = parser.parse(l('abdef'), None)
+        self.assertEqual(m.zero, v2)
+    
+    def testAny(self):
+        p1 = p.any([p.literal(1), p.literal(2)])
+        self.assertEqual(good(l([3,4]), None, 1), p1.parse(l([1,3,4]), None))
+        self.assertEqual(good(l([3,4]), None, 2), p1.parse(l([2,3,4]), None))
+        self.assertEqual(m.zero, p1.parse(l([3,3,4]), None))
+        p2 = p.any([p.literal(1), p.error('oops')])
+        self.assertEqual(good(l([3,4]), None, 1), p2.parse(l([1,3,4]), None))
+        self.assertEqual(m.error('oops'), p2.parse(l([2,3,4]), None))
+    
+    def testZero(self):
+        self.assertEqual(m.zero, p.zero.parse(None, None))
+    
+    def testItem(self):
+        self.assertEqual(p.item.parse(l(range(5)), {}), good(l([1,2,3,4]), {}, 0))
         
+    def testGet(self):
+        self.assertEqual(p.get.parse('abc', {}), good('abc', {}, 'abc'))
+    
+    def testGetState(self):
+        self.assertEqual(good('abc', 123, 123), p.getState.parse('abc', 123))
+    
