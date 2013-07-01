fe3bbbc4463409d46aabb4d4f00d5ac5d57ed637
Matt Fenwick <mfenwick100@gmail.com>
2013-07-01 11:08:01 -0400

    remove 'c.' prefix from json parsers

--- unparse/examples/json.py
+++ unparse/examples/json.py
@@ -16,23 +16,28 @@
 #         escape  :=  '\\'  ["\/bfnrt]                            <-- that's 8 characters
 #         unicode :=  '\\u'  [0-9a-eA-E](4)
 
-from .. import combinators as c
+from ..combinators import (bind,   getState, commit,   mapError,
+                           many0,  app,      optional, plus,
+                           seq2R,  many1,    fmap,     pure,
+                           zero,   all_,     any_,     error,
+                           seq2L,  itemPosition, tokenPosition,
+                           not0)
 
 
-item = c.itemPosition
-(literal, satisfy, not1, string) = c.tokenPosition
+item = itemPosition
+(literal, satisfy, not1, string) = tokenPosition
 
 def cut(message, parser):
-    return c.bind(c.getState, lambda p: c.commit([(message, p)], parser))
+    return bind(getState, lambda p: commit([(message, p)], parser))
 
 def addError(e, parser):
-    return c.bind(c.getState,
-                  lambda pos: c.mapError(lambda es: [(e, pos)] + es, parser))
+    return bind(getState,
+                lambda pos: mapError(lambda es: [(e, pos)] + es, parser))
 def oneOf(cs):
     return satisfy(lambda x: x in cs)
 
 
-whitespace = c.many0(oneOf(set(' \t\n\r')))
+whitespace = many0(oneOf(set(' \t\n\r')))
 
 def numberAction(sign, intp, frac, exp):
     '''
@@ -47,14 +52,14 @@ def numberAction(sign, intp, frac, exp):
         myString += exp
     return float(myString)
 
-number = c.app(numberAction,
-               c.optional('+', literal('-')),
-               c.plus(literal('0'), c.app(lambda y, ys: ''.join([y] + ys), oneOf('123456789'), c.many0(oneOf('0123456789')))),
-               c.optional(None, c.seq2R(literal('.'), c.fmap(''.join, c.many1(oneOf('0123456789'))))),
-               c.optional(None, c.app(lambda x,y,z: ''.join([x,y,''.join(z)]), 
-                                      oneOf('eE'), 
-                                      c.optional('+', oneOf('+-')),
-                                      c.many1(oneOf('0123456789')))))
+number = app(numberAction,
+             optional('+', literal('-')),
+             plus(literal('0'), app(lambda y, ys: ''.join([y] + ys), oneOf('123456789'), many0(oneOf('0123456789')))),
+             optional(None, seq2R(literal('.'), fmap(''.join, many1(oneOf('0123456789'))))),
+             optional(None, app(lambda x,y,z: ''.join([x,y,''.join(z)]), 
+                                oneOf('eE'), 
+                                optional('+', oneOf('+-')),
+                                many1(oneOf('0123456789')))))
 
 # is it an error if 0 <= c <= 31 ??
 _char = satisfy(lambda x: 32 <= ord(x) and x not in '\\"')
@@ -66,52 +71,52 @@ _escapes = {'"': '"',  '\\': '\\',
 
 def _escapeAction(x):
     if _escapes.has_key(x):
-        return c.pure(_escapes[x])
-    return c.zero
+        return pure(_escapes[x])
+    return zero
 
-_escape = c.seq2R(literal('\\'),
-                  c.bind(item, _escapeAction))
+_escape = seq2R(literal('\\'),
+                bind(item, _escapeAction))
 
 _hexC = satisfy(lambda x: x in set('0123456789abcdefABCDEF'))
 
-_unic = c.app(lambda _, cs: unichr(int(''.join(cs), 16)),
-             string('\\u'),
-             cut('invalid hex escape sequence', c.all_([_hexC] * 4)))
+_unic = app(lambda _, cs: unichr(int(''.join(cs), 16)),
+           string('\\u'),
+           cut('invalid hex escape sequence', all_([_hexC] * 4)))
 
-jsonstring = c.app(lambda _1, cs, _2: ''.join(cs),
-                   literal('"'),
-                   c.many0(c.any_([_char, _escape, _unic])),
+jsonstring = app(lambda _1, cs, _2: ''.join(cs),
+                 literal('"'),
+                 many0(any_([_char, _escape, _unic])),
                    cut('string: expected "', literal('"')))
 
-boolean = c.plus(c.seq2R(string('true'), c.pure(True)),
-                 c.seq2R(string('false'), c.pure(False)))
+boolean = plus(seq2R(string('true'), pure(True)),
+               seq2R(string('false'), pure(False)))
 
-null = c.seq2R(string('null'), c.pure(None))
+null = seq2R(string('null'), pure(None))
 
 
 # hack to allow mutual recursion of rules
-obj = c.error('unimplemented')
-array = c.error('unimplemented')
+obj = error('unimplemented')
+array = error('unimplemented')
 
 def tok(parser):
-    return c.seq2L(parser, whitespace)
+    return seq2L(parser, whitespace)
 
 os, cs, oc, cc, comma, colon = map(lambda x: tok(literal(x)), '[]{},:')
 
-value = c.any_([tok(jsonstring), tok(number), tok(boolean), tok(null), obj, array])
+value = any_([tok(jsonstring), tok(number), tok(boolean), tok(null), obj, array])
 
 
 
 def between(opener, body, closer, message):
-    return c.app(lambda _1, bs, _2: bs, 
-                 opener,
-                 body,
-                 cut(message, closer))
+    return app(lambda _1, bs, _2: bs, 
+               opener,
+               body,
+               cut(message, closer))
 
 def sepBy0(parser, separator):
-    return c.optional([], c.app(lambda x, xs: [x] + xs,
-                                parser,
-                                c.many0(c.seq2R(separator, parser))))
+    return optional([], app(lambda x, xs: [x] + xs,
+                            parser,
+                            many0(seq2R(separator, parser))))
 
 
 array.parse = addError('array',
@@ -121,10 +126,10 @@ array.parse = addError('array',
                                'expected ]')).parse
 
 keyVal = addError('key/value pair',
-                  c.app(lambda k, _, v: (k, v),
-                        tok(jsonstring),
-                        cut('expected :', colon),
-                        cut('expected value', value)))
+                  app(lambda k, _, v: (k, v),
+                      tok(jsonstring),
+                      cut('expected :', colon),
+                      cut('expected value', value)))
 
 obj.parse = addError('object',
                      between(oc,
@@ -132,7 +137,7 @@ obj.parse = addError('object',
                              cc,
                              'expected }')).parse
 
-json = c.app(lambda _1, v, _2: v,
-             whitespace, 
-             value, # should just be object or array according to RFC 4627
-             cut('unparsed input remaining', c.not0(item)))
+json = app(lambda _1, v, _2: v,
+           whitespace, 
+           value, # should just be object or array according to RFC 4627
+           cut('unparsed input remaining', not0(item)))
