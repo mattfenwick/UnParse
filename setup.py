2ef8d968fd4484d49134fc0b09b6fdb7d970f86e
Matt Fenwick <mfenwick100@gmail.com>
2013-11-19 10:30:41 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='UnParse',
-    version='0.0.5',
+    version='0.0.6',
     packages=['unparse', 'unparse.examples'],
     license='MIT',
     author='Matt Fenwick',
