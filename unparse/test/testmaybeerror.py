ca9e625cd7def5b0642c351ac73030e5f694b49a
Matt Fenwick <mfenwick100@gmail.com>
2014-01-26 11:56:20 -0500

    standardize and simplify maybeerror tests, to keep in sync with UnParse-js

--- unparse/test/testmaybeerror.py
+++ unparse/test/testmaybeerror.py
@@ -2,97 +2,95 @@ from .. import maybeerror
 import unittest as u
 
 
-
 m = maybeerror.MaybeError
 
 def inc(x):
     return x + 1
 
+def f_b(x):
+    if x == 3:
+        return m.pure(x + 1)
+    elif x == 4:
+        return m.zero
+    return m.error('e1')
+
+def f_a(x, y, z):
+    return [x, z, y, z]
+
+g1, g2, g3, g4 = map(m.pure, ['g1', 'g2', 3, 4])
+e1, e2, e3, e4 = map(m.error, ['e1', 'e2', 3, 4])
+b1 = m.zero
+
 
 class Tests(u.TestCase):
 
     def testPure(self):
-        myP = m.pure(3)
-        self.assertEqual(myP.value, 3)
-        self.assertEqual(myP.status, 'success')
+        self.assertEqual(g1.value, 'g1')
+        self.assertEqual(g1.status, 'success')
     
     def testZero(self):
-        myZ = m.zero
-        self.assertEqual(myZ.value, None)
-        self.assertEqual(myZ.status, 'failure')
+        self.assertEqual(b1.value, None)
+        self.assertEqual(b1.status, 'failure')
     
     def testError(self):
-        myE = m.error('hi')
-        self.assertEqual(myE.value, 'hi')
-        self.assertEqual(myE.status, 'error')
+        self.assertEqual(e1.value, 'e1')
+        self.assertEqual(e1.status, 'error')
+
+    def testConstructors(self):
+        self.assertEqual(g1, m('success', 'g1'))
+        self.assertEqual(b1, m('failure', None))
+        self.assertEqual(e1, m('error', 'e1'))
     
     def testEquality(self):
-        self.assertEqual(m.pure(3), m.pure(3))
-        self.assertEqual(m.zero, m.zero)
-        self.assertEqual(m.error('hi'), m.error('hi'))
+        self.assertEqual(g1, g1)
+        self.assertEqual(b1, b1)
+        self.assertEqual(e1, e1)
         
     def testInequality(self):
-        self.assertNotEqual(m.pure(3), m.pure(4))
-        self.assertNotEqual(m.pure(3), m.zero)
-        self.assertNotEqual(m.pure(3), m.error(3))
-        self.assertNotEqual(m.zero, m.pure(4))
-        self.assertNotEqual(m.zero, m.error('hi'))
-        self.assertNotEqual(m.error(3), m.pure(3))
-        self.assertNotEqual(m.error('hi'), m.zero)
-        self.assertNotEqual(m.error('hi'), m.error('bye'))
+        self.assertNotEqual(g1, g2)
+        self.assertNotEqual(g1, b1)
+        self.assertNotEqual(g1, e1)
+        self.assertNotEqual(b1, g1)
+        self.assertNotEqual(b1, e1)
+        self.assertNotEqual(e1, g1)
+        self.assertNotEqual(e1, b1)
+        self.assertNotEqual(e1, e2)
 
     def testFmap(self):
-        good, bad, error = m.pure(3), m.zero, m.error('oops')
-        self.assertEqual(good.fmap(inc), m.pure(4))
-        self.assertEqual(bad.fmap(inc), bad)
-        self.assertEqual(error.fmap(inc), error)
-        self.assertEqual(good.status, 'success')
-        self.assertEqual(good.value, 3)
+        self.assertEqual(g3.fmap(inc), g4)
+        self.assertEqual(b1.fmap(inc), b1)
+        self.assertEqual(e3.fmap(inc), e3)
     
     def testBind(self):
-        g1, g2, g3, b, e = m.pure(1), m.pure(4), m.pure(8), m.zero, m.error('oops')
-        def f(x):
-            if x > 5:
-                return m.pure(x + 3)
-            elif 3 < x <= 5:
-                return m.error('bad x: ' + str(x))
-            return m.zero
-        self.assertEqual(g1.bind(f), b)
-        self.assertEqual(g2.bind(f), m.error('bad x: 4'))
-        self.assertEqual(g3.bind(f), m.pure(11))
-        self.assertEqual(b.bind(f), b)
-        self.assertEqual(e.bind(f), e)
+        self.assertEqual(g2.bind(f_b), e1)
+        self.assertEqual(g3.bind(f_b), g4)
+        self.assertEqual(g4.bind(f_b), b1)
+        self.assertEqual(b1.bind(f_b), b1)
+        self.assertEqual(e1.bind(f_b), e1)
     
     def testApp(self):
-        g1, g2, g3 = map(m.pure, [3, 4, 5])
-        z1, e1 = m.zero, m.error('oops')
-        def f(x, y, z):
-            return [x, z, y, z]
         # apply over lots of success
-        self.assertEqual(m.app(f, g1, g2, g3), m.pure([3, 5, 4, 5]))
+        self.assertEqual(m.app(f_a, g1, g2, g3), m.pure(['g1', 3, 'g2', 3]))
         # short-circuit zero
-        self.assertEqual(m.app(f, g1, z1, g2), z1)
+        self.assertEqual(m.app(f_a, g1, b1, g2), b1)
         # short-circuit error
-        self.assertEqual(m.app(f, g1, g3, e1), e1)
+        self.assertEqual(m.app(f_a, g1, g3, e1), e1)
 
     def testPlus(self):
-        g1, g2, bad, err = m.pure(4), m.pure('hi'), m.zero, m.error('oops')
         # good + x -> good (left-biased)
         self.assertEqual(g1.plus(g2), g1)
-        self.assertEqual(g2.plus(g1), g2)
-        self.assertEqual(g1.plus(bad), g1)
-        self.assertEqual(g1.plus(err), g1)
+        self.assertEqual(g1.plus(b1), g1)
+        self.assertEqual(g1.plus(e1), g1)
         # bad + x -> x
-        self.assertEqual(bad.plus(g1), g1)
-        self.assertEqual(bad.plus(bad), bad)
-        self.assertEqual(bad.plus(err), err)
+        self.assertEqual(b1.plus(g1), g1)
+        self.assertEqual(b1.plus(b1), b1)
+        self.assertEqual(b1.plus(e1), e1)
         # error + x -> error (left-biased)
-        self.assertEqual(err.plus(g1), err)
-        self.assertEqual(err.plus(bad), err)
-        self.assertEqual(err.plus(m.error('another error')), err)
+        self.assertEqual(e1.plus(g1), e1)
+        self.assertEqual(e1.plus(b1), e1)
+        self.assertEqual(e1.plus(e2), e1)
 
     def testMapError(self):
-        good, bad, error = m.pure(3), m.zero, m.error(4)
-        self.assertEqual(good.mapError(inc) , good)
-        self.assertEqual(bad.mapError(inc)  , bad)
-        self.assertEqual(error.mapError(inc), m.error(5))
+        self.assertEqual(g1.mapError(inc), g1)
+        self.assertEqual(b1.mapError(inc), b1)
+        self.assertEqual(e3.mapError(inc), e4)
