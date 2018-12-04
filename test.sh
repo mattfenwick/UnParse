f3747f15c6603b787a5746518d0c32c45d26702c
Matt Fenwick <mfenwick100@gmail.com>
2018-12-04 07:34:05 -0500

    run all tests

--- test.sh
+++ test.sh
@@ -3,3 +3,7 @@ python -m unparse.test.testconslist
 python -m unparse.test.testcst
 python -m unparse.test.testmaybeerror
 python -m unparse.test.testfunctions
+
+python -m unparse.test.testexamples.testjavatokens
+python -m unparse.test.testexamples.testjson
+python -m unparse.test.testexamples.testjsontree
